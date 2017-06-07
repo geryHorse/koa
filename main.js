c48e3cac65133b4d09456b04524151d5e2e204ea
mahui <mahui00000@163.com>
2017-06-08 00:12:23 +0800

    目录修改,  文件模块化

---
+++ main.js
@@ -0,0 +1,26 @@
+
+const Koa = require('koa');
+
+const bodyParser = require('koa-bodyparser');
+
+
+const app = new Koa();
+
+const controller = require('./controller');
+
+
+app.use(bodyParser());
+
+app.use(controller());
+
+app.listen(3000);
+
+console.log('app started at port 3000...');
+
+
+
+
+
+
+
+
